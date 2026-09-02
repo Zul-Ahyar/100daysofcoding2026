3b0a821c522a4434a6def6f731011908460f3e6c
Zul-Ahyar <nanya7906@gmail.com>
2026-09-02 18:01:40 +0800

    Day0.java

---
+++ Day0.java
@@ -0,0 +1,8 @@
+public class gacor{
+  public static void main (String[] args){
+    String nama = "Zuljhy";
+    int umur = 19;
+    System.out.println("Nama saya: " + nama);
+    System.out.println("Umur saya: " + umur);
+  }
+}
