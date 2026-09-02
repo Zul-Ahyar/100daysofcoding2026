9b3a30b3e9374e31d81a4f43da26c744753cb63f
Zul-Ahyar <nanya7906@gmail.com>
2026-09-02 22:41:19 +0800

    Day1.java

---
+++ Day1.java
@@ -0,0 +1,20 @@
+/**
+ * Program Day 1
+ * Belajar struktur dasar Java dan jenis-jenis komentar
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Ini adalah komentar satu baris
+        System.out.println("Halo, ISC!");
+
+        /*
+         Ini adalah komentar
+         yang terdiri dari beberapa baris
+        */
+
+        System.out.println("Day 1 dari 100 Hari Ngoding!");
+    }
+}
