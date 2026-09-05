df3036993ec151862f367650f4b7cba7cbe2cee9
Zul-Ahyar <nanya7906@gmail.com>
2026-09-05 19:16:26 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,24 @@
+public class Day4 {
+
+    public static void main(String[] args) {
+
+        // byte untuk angka yang kecil.
+        byte umur = 19;
+
+        // short untuk angka yang lebih besar dari byte.
+        short tahunKelahiran = 2007;
+
+        // int untuk angka yang lebih besar lagi.
+        int jumlahPenduduk = 300000;
+
+        // long untuk angka yang sangat besar.
+        // L menandakan kalau angka tersebut bertipe long.
+        long uang = 6281215857213L;
+
+        // println digunakan untuk menampilkan output lalu pindah ke baris berikutnya.
+        System.out.println("Umur saya: " + umur);
+        System.out.println("Saya kelahiran: " + tahunKelahiran);
+        System.out.println("Jumlah penduduk: " + jumlahPenduduk);
+        System.out.println("Saya memiliki uang: " + uang);
+    }
+}
