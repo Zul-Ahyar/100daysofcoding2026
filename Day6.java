815b96090213962ba746c12a654357e0d7f5360d
Zul-Ahyar <nanya7906@gmail.com>
2026-09-07 20:44:43 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,20 @@
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        // char digunakan untuk menyimpan satu karakter. 
+        // Kemudian karakter ditulis menggunakan tanda petik satu (' ').
+        char inisialSaya ='Z';
+
+        // boolean digunakan untuk menyimpan nilai benar atau salah.
+        // Nilainya itu hanya bisa true atau false.
+        boolean benar = true;  
+        
+        // printf digunakan untuk menampilkan output dengan format tertentu.
+        // %c digunakan untuk menampilkan nilai char.
+        // %b digunakan untuk menampilkan nilai boolean. 
+        // %n digunakan untuk pindah ke baris berikutnya.
+        System.out.printf("Inisial saya adalah: %c%n", inisialSaya);
+        System.out.printf("Apakah benar inisial adalah Z: %b%n", benar);
+    }
+}
