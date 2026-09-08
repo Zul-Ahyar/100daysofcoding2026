9cdb0596cbc39646a037a7cebfa4b466b23fb2ed
Zul-Ahyar <nanya7906@gmail.com>
2026-09-08 20:53:38 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,11 @@
+public class Day7 {
+
+    public static void main(String[] args) {
+        // Membuat variabel myIdol dengan tipe data String karena myIdol berupa teks/karakter.
+        String myIdol = "Cristiano Ronaldo";
+       
+       // println digunakan untuk menampilkan output lalu pindah ke baris berikutnya.
+        System.out.printf("Nama idola saya: " + myIdol);
+         
+    }
+}
