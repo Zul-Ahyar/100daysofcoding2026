af5b30f43b65cc1324323841bd08440373a9cca4
Zul-Ahyar <nanya7906@gmail.com>
2026-09-10 21:12:37 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,17 @@
+public class Day9 {
+
+    public static void main(String[] args) {
+        
+        // Membuat variabel kampus pakai tipe data String isi "UNSULBAR", dikunci pakai final biar tidak bisa diubah
+        final String kampus = "UNSULBAR";
+        
+        // Membuat variabel jumlahBulan pakai tipe data int isi 12, dikunci juga nilainya
+        final int jumlahBulan = 12;
+
+        // println digunakan untuk menampilkan output lalu pindah ke baris berikutnya.
+        System.out.println("Nama kampus tercinta: " + kampus);
+        
+        // println digunakan untuk menampilkan output lalu pindah ke baris berikutnya.
+        System.out.println("Jumlah bulan: " + jumlahBulan);
+    }
+}
