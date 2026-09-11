55e9b21fab653024d40d2f247622fb6768f6a237
Zul-Ahyar <nanya7906@gmail.com>
2026-09-11 21:19:14 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,17 @@
+public class Day10 {
+
+    public static void main(String[] args) {
+        // Menampilkan batas nilai minimum dan maksimum dari tipe data integer di Java
+        System.out.println("Nilai minimum byte  : " + Byte.MIN_VALUE);
+        System.out.println("Nilai maksimum byte  : " + Byte.MAX_VALUE);
+
+        System.out.println("Nilai minimum short : " + Short.MIN_VALUE);
+        System.out.println("Nilai maksimum short : " + Short.MAX_VALUE);
+
+        System.out.println("Nilai minimum int   : " + Integer.MIN_VALUE);
+        System.out.println("Nilai maksimum int   : " + Integer.MAX_VALUE);
+
+        System.out.println("Nilai minimum long  : " + Long.MIN_VALUE);
+        System.out.println("Nilai maksimum long  : " + Long.MAX_VALUE);
+    }
+}
