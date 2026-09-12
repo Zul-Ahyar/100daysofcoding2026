a05e087ecf78be1bed2f824a64b1757ddb5f8f1f
Zul-Ahyar <nanya7906@gmail.com>
2026-09-12 22:44:06 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,18 +1,35 @@
-import java.util.Scanner; // Mengimpor kelas Scanner dari paket java.util
+import java.util.Scanner;
 
 public class Day11 {
-
     public static void main(String[] args) {
-        // Membuat objek Scanner untuk membaca input dari keyboard
         Scanner input = new Scanner(System.in);
 
-        // Menampilkan pesan petunjuk (prompt) ke konsol
-        System.out.print("Masukkan nama anda: ");
-        
-        // Membaca teks satu baris penuh yang diinput pengguna
+        // Input 9 Tipe Data
+        System.out.println("Masukkan Nama Lengkap:");
         String nama = input.nextLine();
 
-        // Menampilkan hasil input menggunakan println
-        System.out.println("Nama saya: " + nama);
+        System.out.println("Masukkan Umur:");
+        int umur = input.nextInt();
+
+        System.out.println("Masukkan Tinggi Badan:");
+        double tb = input.nextDouble();
+
+        System.out.println("Masukkan Berat Badan:");
+        float bb = input.nextFloat();
+        
+        System.out.println("Masukkan ID Anggota:");
+        short idMember = input.nextShort();
+
+        System.out.println("Masukkan Jumlah Saudara Kandung:");
+        byte jumlahSaudara = input.nextByte();
+
+        System.out.println("Masukkan NIK:");
+        long nik = input.nextLong();
+
+        System.out.println("Masukkan Golongan Darah:");
+        char golDarah = input.next().charAt(0);
+
+        System.out.println("Apakah Status Mahasiswa Aktif? (true/false):");
+        boolean isAktif = input.nextBoolean();
     }
 }
