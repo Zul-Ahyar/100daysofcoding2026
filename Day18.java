624063c583b920705b00b31334e1c2145ccfef63
Zul-Ahyar <nanya7906@gmail.com>
2026-09-19 19:43:09 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,8 @@
+public class Day18 {
+    public static void main(String[] args) {
+        byte umur = 19;
+        int hasil = umur;
+        System.out.println("Umur: " + hasil + " Tahun");
+
+    }
+}
