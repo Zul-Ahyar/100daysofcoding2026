e8743a2629d9d272bcfad0bdeebbaab5b90c05c3
Zul-Ahyar <nanya7906@gmail.com>
2026-09-20 21:11:25 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,7 @@
+public class Day19 {
+    public static void main(String[] args) {
+        double tinggi = 166.6;
+        int hasil = (int) tinggi;
+        System.out.println("Tinggi: " + hasil);
+    }
+}
