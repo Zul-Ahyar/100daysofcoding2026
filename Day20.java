eeec2f497ecfa33d23568d122f06b8ca455660a7
Zul-Ahyar <nanya7906@gmail.com>
2026-09-21 21:03:58 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Day20 {
+    public static void main(String[] args) {
+        char inisial = 'Z';
+        String hasil = String.valueOf(inisial);
+        System.out.println("Inisial: " + hasil);
+
+    }
+}
