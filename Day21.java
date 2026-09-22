6e851e6f8a27bef9195ac0b1ef4ec7929e0e9e0c
Zul-Ahyar <nanya7906@gmail.com>
2026-09-22 21:29:21 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,7 @@
+public class Day21 {
+    public static void main(String[] args) {
+        String angka = "19";
+        int umur = Integer.parseInt(angka);
+        System.out.println("Umur: " + umur);
+    }
+}
