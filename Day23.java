bd2550452a6331d2ec27b4e1ad6a5fc9a5ccc0f5
Zul-Ahyar <nanya7906@gmail.com>
2026-09-24 20:16:45 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int sisi = 4;
+        int luas = sisi * sisi;
+        System.out.println("Luas: " + luas);
+        
+    }
+}
