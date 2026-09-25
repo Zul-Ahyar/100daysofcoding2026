e4a03792bd113d02ae3a763fa0d02ab1a41e9d55
Zul-Ahyar <nanya7906@gmail.com>
2026-09-25 19:47:48 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,10 @@
+public class Day24 {
+    public static void main(String[] args) {
+        int panjang = 6;
+        int lebar = 4;
+
+        int luas = panjang * lebar;
+        
+        System.out.println("Luas: " + luas);
+    }
+}
